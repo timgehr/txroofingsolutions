8edff61ab860a38ad896a3e9b0c5f0987a39a6ac
Tim Gehrsitz <36209570+twgehrsitz@users.noreply.github.com>
2020-07-30 02:46:27 -0500

    blog link on blog page

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="page blog">
-    Blog
+  <div class="page bloghub">
+    <router-link to="blog/common-types-of-roof-leaks">Post 1</router-link>
+    <router-link to="blog/common-types-of-roof-leaks/edit">Edit Post 1</router-link>
   </div>
 </template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
